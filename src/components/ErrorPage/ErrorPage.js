4214038189efaac1f12a5a571d57f5f4aacdbfab
Oryam <48219965+or-yam@users.noreply.github.com>
2021-08-09 18:05:08 +0300

    Add error page

---
+++ src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,11 @@
+import vader from '../../assets/vader.png';
+import styles from './ErrorPage.module.css';
+
+export default function ErrorPage() {
+  return (
+    <div className={styles.container}>
+      <h1>Something went wrong</h1>
+      <img src={vader} alt="darth vader face" />
+    </div>
+  );
+}
