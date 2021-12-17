d1bfccb8e8119129ff115544e879f3050cbd4d47
Oryam <48219965+or-yam@users.noreply.github.com>
2021-12-17 08:12:13 +0200

    Add logs to localStorage service

--- src/services/localStorage.js
+++ src/services/localStorage.js
@@ -2,11 +2,12 @@ const STORAGE_NAME = 'swapi_react_results';
 
 export const saveResultsToLocalStorage = (results, key) => {
   localStorage.setItem(`${STORAGE_NAME}_${key}`, JSON.stringify(results));
+  console.info(`${key} Saved to localStorage`);
 };
 
 export const getResultsFromLocalStorage = key => {
   const results = localStorage.getItem(`${STORAGE_NAME}_${key}`);
   if (!results) return;
-  console.log(`${key} restored from local storage`);
+  console.info(`${key} restored from local storage`);
   return JSON.parse(results);
 };
